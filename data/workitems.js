9bf046c908f8028db4e00506fda75baf73bcce7f
webpropopuli <webpropopuli@gmail.com>
2019-04-22 14:45:34 -0400

    more

--- data/workitems.js
+++ data/workitems.js
@@ -4,7 +4,7 @@ const workItems = [
   [0, "Fill in Portfolio items with real data"],
   [0, "Move Portfolio to Mongo"],
   [1, "Deploy to live site", "22April19"],
-  [(0, `Make 'real' pages in menu`)],
+  [0, `Make 'real' pages in menu`],
   [0, `Style the menu as proper Nav`],
   [1, `Make workList dynamic`],
   [0, "Fix this awful styling"]
